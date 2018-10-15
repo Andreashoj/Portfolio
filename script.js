09be91f63f2491889a01458018844981db7ca99c
Andreas Hoj <andrewhoj@gmail.com>
2018-10-15 19:37:59 +0200

    mobile navbar animated

--- script.js
+++ script.js
@@ -3,22 +3,25 @@ let menuSwitch = true;
 const menu = document.querySelector('.menu');
 const menuLi = document.querySelector('.mobilList')
 const menuSvg = document.querySelectorAll('.mobile-menu-svg');
-document.querySelector('.fa').addEventListener('click', function(){
+let burgerAnime = document.querySelectorAll('.toggle span')
+document.querySelector('.toggle').addEventListener('click', function(){
     if (menuSwitch == true) { 
         menu.style.width = '70%';
         menuSwitch = false;
         menuLi.style.opacity = 1;
         for(var i = 0; i < menuSvg.length; i++) {
             menuSvg[i].style.opacity = 1;
+            burgerAnime[i] = burgerAnime[i].className = 'burger-anime'
         }
-    } else { 
-        menu.style.width = '0%';
-        menuSwitch = true;
-        menuLi.style.opacity = 0;
-        for(var i = 0; i < menuSvg.length; i++) {
-            menuSvg[i].style.opacity = 0;
+        } else { 
+            menu.style.width = '0%';
+            menuSwitch = true;
+            menuLi.style.opacity = 0;
+            for(var i = 0; i < menuSvg.length; i++) {
+                menuSvg[i].style.opacity = 0;
+                burgerAnime[i] = burgerAnime[i].className = ''
+            }
         }
-    }
 })
 
 window.addEventListener('resize', function(){
@@ -28,6 +31,7 @@ window.addEventListener('resize', function(){
         menuLi.style.opacity = 0;
         for(var i = 0; i < menuSvg.length; i++) {
             menuSvg[i].style.opacity = 0;
+            burgerAnime[i] = burgerAnime[i].className = ''
         }
     }
 
